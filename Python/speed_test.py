9ab5b9ca00d23cb63b8ac2c9cf55ad5cbf12f78b
dpbenalcazar <danielbenalcazar87@gmail.com>
2022-05-14 07:17:54 -0500

    Speed test

--- Python/speed_test.py
+++ Python/speed_test.py
@@ -18,11 +18,9 @@ data = df.drop(columns=['ID']).to_numpy()
 pupil_xyr = data[2, 0:3]
 iris_xyr  = data[2, 3:6]
 
-# Input dilation
-dil1 = pupil_xyr[2]/iris_xyr[2]
-
 # Output set of 100 dilation levels
-dil2_set = np.linspace(0.1, 0.8, 100)
+N_imgs = 100
+dil2_set = np.linspace(0.1, 0.8, N_imgs)
 
 # Measure time for 100 images
 t0 = time.time()
@@ -33,6 +31,6 @@ dt = time.time()-t0
 # Print results
 print('\nTest results: ')
 print('  -- Image size: {}'.format(im1.size))
-print('  -- Time for 100 predictions: {:0.3f} s'.format(dt))
-print('  -- Average time per image: {:0.3f} ms'.format(10*dt))
-print('  -- Speed: {:0.1f} images per second'.format(100/dt))
+print('  -- Time for {} images: {:0.3f} s'.format(N_imgs, dt))
+print('  -- Average time per image: {:0.3f} ms'.format(1000*dt/N_imgs))
+print('  -- Speed: {:0.1f} images per second'.format(N_imgs/dt))
